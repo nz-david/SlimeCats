b4d3d2f0b7c994b01dc02ef49c949e7ad53c0dd2
starlitclover <batzerv@gmail.com>
2025-03-14 17:59:13 -0400

    some of the idle animations
    the ones where the cat is facing away have not been added yet

--- Scenes/Player/TestCat.gd
+++ Scenes/Player/TestCat.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var cat_angle = 0
 var SPEED = 300.0
 @onready var cat_run = $AnimatedSprite2D
 
@@ -13,29 +13,43 @@ func _physics_process(delta: float) -> void:
 		cat_run.flip_h=false
 		if is_down_pressed:
 			cat_run.play("3_4 view forward run")
+			cat_angle = 0
 		elif is_up_pressed:
 			cat_run.play("3_4 view backward run")
+			cat_angle = 1
 		elif is_left_pressed:
-			cat_run.stop()
+			cat_run.play("side view idle")
 		else:
 			cat_run.play("side view run")
+			cat_angle = 2
 	elif is_left_pressed:
 		cat_run.flip_h=true
 		if is_down_pressed:
 			cat_run.play("3_4 view forward run")
+			cat_angle = 3
 		elif is_up_pressed:
 			cat_run.play("3_4 view backward run")
+			cat_angle = 4
 		else:
 			cat_run.play("side view run")
+			cat_angle = 5
 	elif is_down_pressed:
 		if is_up_pressed:
-			cat_run.stop()
+			cat_run.play("forward view idle")
 		else:
 			cat_run.play("forward view run")
+			cat_angle = 6
 	elif is_up_pressed:
 		cat_run.play("backward view run")
+		cat_angle = 7
 	else:
-		cat_run.stop()
+		match cat_angle:
+			0: cat_run.play("3_4 view forward idle")
+			2: cat_run.play("side view idle")
+			3: 
+				cat_run.play("3_4 view forward idle")
+			5: cat_run.play("side view idle")
+			6: cat_run.play("forward view idle")
 	var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	if direction:
 		velocity = direction * SPEED
