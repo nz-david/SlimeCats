9d389509ed71d2a874732525a86b12d854eb5a57
starlitclover <batzerv@gmail.com>
2025-04-22 11:25:08 -0400

    Collision, Ripple's area, and file cleanup
    you can't run into walls anymore, Ripple's area in the den has things in it (and some animated details! (although one of them has to be redone)), and I deleted the random images that we used to get the collision in since they're not needed anymore

--- den_scene.gd
+++ den_scene.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+@onready var ripple_drip = $AnimatedSprite2D
+@onready var ripple_pool = $RippleRipples
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	can_attack.can_attack = false
+	ripple_drip.play("default")
+	ripple_pool.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
