41960039b998e9baf0e7d92e99832e6b07cd71da
Gabe H. <Gabe.harrison9907@gmail.com>
2025-05-18 16:41:44 -0400

    wave end transition changed
    player should be able to enter den in overworld after wave 10

--- Scenes/Player/test.gd
+++ Scenes/Player/test.gd
@@ -10,6 +10,7 @@ var spawningspeed = 2
 var wave = 1
 @export var enemy_scene: PackedScene
 @onready var timer = $Timer
+@onready var den = $"%Den"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,27 +26,31 @@ func _process(delta: float) -> void:
 		enemydefeated = 0
 		wave += 1
 	
-
+	if Global.incoming == true and wave >= 10:
+		Global.incoming = false
+		den.open = true
+	
 func OnKilled():
 	$AudioStreamPlayer2D2.play()
 
 func _on_timer_timeout() -> void:
-	if(enemyammount > 0 and enemyexisting <= 4):
-		# Create a new instance of the Mob scene.
-		var mob = enemy_scene.instantiate()
-		# Choose a random location on Path2D.
-		var mob_spawn_location = $Path2D/PathFollow2D
-		mob_spawn_location.progress_ratio = randf()
-		# Set the mob's position to the random location.
-		mob.position = mob_spawn_location.position
-		# Spawn the mob by adding it to the Main scene.
-		add_child(mob)
-		enemyexisting += 1
-		enemyammount -= 1
-	else:
-		timer.stop()
-		#wave += 1
-		get_tree().create_timer(5).timeout.connect(wave_start)
+	if Global.incoming:
+		if(enemyammount > 0 and enemyexisting <= 4):
+			# Create a new instance of the Mob scene.
+			var mob = enemy_scene.instantiate()
+			# Choose a random location on Path2D.
+			var mob_spawn_location = $Path2D/PathFollow2D
+			mob_spawn_location.progress_ratio = randf()
+			# Set the mob's position to the random location.
+			mob.position = mob_spawn_location.position
+			# Spawn the mob by adding it to the Main scene.
+			add_child(mob)
+			enemyexisting += 1
+			enemyammount -= 1
+		else:
+			timer.stop()
+			#wave += 1
+			get_tree().create_timer(5).timeout.connect(wave_start)
 
 func wave_start():
 	ammountspawning = 10 + ((wave - 1) * 5)
