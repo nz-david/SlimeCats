801cb7c3b48473ed8ee52dce9e14c420012c6fc0
XiNNiW <david.minnix@protonmail.com>
2025-05-18 16:38:28 -0400

    new music now plays as waves advance

--- Scenes/Player/test.gd
+++ Scenes/Player/test.gd
@@ -15,6 +15,7 @@ var wave = 1
 func _ready() -> void:
 	can_attack.can_attack = true
 	Dialogic.start(set_bond_values)
+	wave_start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,10 +25,11 @@ func _process(delta: float) -> void:
 	if enemydefeated >= 5:
 		enemydefeated = 0
 		wave += 1
+		wave_start()
 	
 
 func OnKilled():
-	$AudioStreamPlayer2D2.play()
+	%SFX/EnemyKilledSFX.play()
 
 func _on_timer_timeout() -> void:
 	if(enemyammount > 0 and enemyexisting <= 4):
@@ -45,11 +47,26 @@ func _on_timer_timeout() -> void:
 	else:
 		timer.stop()
 		#wave += 1
-		get_tree().create_timer(5).timeout.connect(wave_start)
+		#get_tree().create_timer(5).timeout.connect(wave_start)
 
 func wave_start():
+	print("WAVE START: ", wave)
 	ammountspawning = 10 + ((wave - 1) * 5)
 	spawningspeed = 2 - (wave * 0.05)
 	enemyammount += ammountspawning
 	timer.set_wait_time(spawningspeed)
 	timer.start()
+	var songs = %Music.get_children()
+	if wave == 1:
+		for song in songs:
+			song.stop()
+		%Music/BattleAttackSong.play()
+	elif wave == 4:
+		for song in songs:
+			song.stop()
+		%Music/ItsHarderAloneButStillPossible.play()
+	elif wave == 8:
+		for song in songs:
+			song.stop()
+		%Music/SoThisIsIt.play()
+		
